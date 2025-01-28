f27f768bcddc5f1316494a77226ae4a65946220a
Alessandro Pinheiro <alessandropsbra@gmail.com>
2025-01-28 20:46:06 -0300

    fix: update hero section

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="bg-smalt p-5 pt-12">
+  <section class="bg-smalt p-5 py-12">
     <h1 class="py-8 text-center text-xl leading-5 text-white">
       {{ $t('pages.home.heroSection.title') }}
     </h1>
-    <div class="-mx-5 h-72">
+
+    <div class="-mx-5 h-60">
       <ClientOnly>
         <swiper-container
-          class="h-72"
+          class="h-60"
           :loop="true"
           :effect="'coverflow'"
           :autoplay="true"
@@ -24,24 +25,30 @@
             scale: 0.85,
           }">
           <swiper-slide v-for="(slide, idx) in slides" :key="idx">
-            <div class="h-72 w-full">
+            <div class="h-60 w-full">
               <NuxtImg
                 :src="slide"
                 height="300"
                 width="300"
-                class="h-72 w-full rounded-3xl object-cover" />
+                class="h-60 w-full rounded-3xl object-cover" />
             </div>
           </swiper-slide>
         </swiper-container>
       </ClientOnly>
     </div>
 
+    <NuxtImg
+      src="/img/hero-background@3x.png"
+      height="35"
+      width="800"
+      class="-ml-20 h-fit w-fit object-contain pt-4" />
+
     <h2 class="py-8 text-center text-xl leading-5 text-white">
       {{ $t('pages.home.heroSection.subtitle') }}
       <b>{{ $t('pages.home.heroSection.subtitleBold') }}</b>
     </h2>
 
-    <div class="mx-auto flex w-full flex-row justify-between">
+    <div class="mx-auto flex w-fit flex-row gap-6">
       <Button variant="inverted">
         {{ $t('pages.home.heroSection.aboutUs') }}
       </Button>
@@ -49,7 +56,7 @@
         {{ $t('pages.home.heroSection.contactUs') }}
       </Button>
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
