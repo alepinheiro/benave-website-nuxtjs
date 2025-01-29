06460ec168ff3231f87b2e3362953f76aa09c76a
Alessandro Pinheiro <alessandropsbra@gmail.com>
2025-01-29 20:42:23 -0300

    fix: update call to action css

--- components/home/CallToAction.vue
+++ components/home/CallToAction.vue
@@ -1,18 +1,22 @@
 <template>
   <section>
-    <h2>
-      {{ $t('pages.home.callToAction.1.title') }}
-    </h2>
-    <div>
-      <h3>
-        {{ $t('pages.home.callToAction.1.tagline') }}
-      </h3>
-      <Button>
-        {{ $t('pages.home.callToAction.1.phone') }}
-      </Button>
-      <Button>
-        {{ $t('pages.home.callToAction.1.email') }}
-      </Button>
+    <div class="bg-primary px-5 py-12">
+      <h2 class="text-center text-2xl font-bold text-white">
+        {{ $t('pages.home.callToAction.1.title') }}
+      </h2>
+    </div>
+    <div class="bg-gradient-to-t from-zinc-500 to-zinc-200 px-5 py-12">
+      <div class="flex flex-col gap-5 rounded-xl bg-zinc-800 p-5 text-white">
+        <h3 class="text-center text-xl font-bold">
+          {{ $t('pages.home.callToAction.1.tagline') }}
+        </h3>
+        <Button variant="inverted" class="mx-auto w-fit text-center">
+          {{ $t('pages.home.callToAction.1.phone') }}
+        </Button>
+        <Button variant="link" class="text-white">
+          {{ $t('pages.home.callToAction.1.email') }}
+        </Button>
+      </div>
     </div>
   </section>
 </template>
