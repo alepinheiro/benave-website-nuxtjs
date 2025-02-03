f7bfe1a8265934a894c8d5964b62f5cd20d59bd6
Alessandro Pinheiro <alessandropsbra@gmail.com>
2025-02-03 20:47:40 -0300

    fix: updates use posts composable

--- composables/usePosts.ts
+++ composables/usePosts.ts
@@ -1,5 +1,17 @@
-import type { WPPost } from "~/types/wordPress"
+import type { WPPost } from '~/types/wordPress';
 
-export const usePosts = () => useFetch<Array<WPPost>>('https://public-api.wordpress.com/wp/v2/sites/alessandropsbra.wordpress.com/posts', {
-  method: 'GET'
-})
+export const usePosts = () =>
+  useAsyncData<Array<WPPost>>(
+    'posts',
+    () =>
+      $fetch(
+        'https://public-api.wordpress.com/wp/v2/sites/alessandropsbra.wordpress.com/posts',
+        {
+          method: 'GET',
+        },
+      ),
+    {
+      server: true, // garante que a requisição será feita no servidor
+      transform: (data) => data, // opcional: você pode transformar os dados aqui se necessário
+    },
+  );
