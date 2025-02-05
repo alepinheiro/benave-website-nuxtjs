01a4645e64b6b06f0b4422391e811f9bcbb9e4d1
Alessandro Pinheiro <alessandropsbra@gmail.com>
2025-02-04 21:18:07 -0300

    fix: updates blog section

--- components/home/TheBLog.vue
+++ components/home/TheBLog.vue
@@ -1,11 +1,15 @@
 <template>
   <section class="blog px-5 py-12">
     <div class="mx-auto flex max-w-7xl flex-col gap-5 md:flex-row">
-      <div v-if="pending">Carregando...</div>
-      <div v-else-if="error">Erro ao carregar posts.</div>
-      <div v-else>
-        <p class="text-red-500">{{ posts }}</p>
-        <p class="text-primary-500">{{ posts }}</p>
+      <div v-if="status === 'pending'">Carregando...</div>
+      <div v-if="status === 'success' && posts && posts.length > 0">
+        <div>
+          <p class="text-red-500" v-html="posts[0].title.rendered"></p>
+          <p class="text-primary-500" v-html="posts[0].title.rendered"></p>
+        </div>
+      </div>
+      <div v-else-if="status === 'error'">
+        {{ error }}
       </div>
     </div>
   </section>
@@ -17,7 +21,7 @@
   // ✅ Use o useFetch diretamente para funcionar no SSR e CSR
   const {
     data: posts,
-    pending,
+    status,
     error,
   } = await useLazyFetch<WPPost[]>('/api/posts');
 
