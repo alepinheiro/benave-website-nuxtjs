ca67e24e51fb659f46bcdb838b128ad87a5b786d
Alessandro Pinheiro <alessandropsbra@gmail.com>
2025-02-09 12:37:12 -0300

    fix: update menu behavior

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtPage vaul-drawer-wrapper class="scroll-smooth" />
+  <NuxtPage />
 </template>
 
 <script setup lang="ts"></script>
--- components/home/TopBar.vue
+++ components/home/TopBar.vue
@@ -1,59 +1,25 @@
 <template>
   <nav class="sticky top-0 z-10 px-2 pt-2">
     <div
-      class="mx-auto h-full w-full max-w-7xl rounded-xl bg-white px-6 py-2 text-primary shadow-md">
+      class="relative z-30 mx-auto h-full w-full max-w-7xl rounded-xl bg-white px-6 py-2 text-primary shadow-md">
       <div class="flex flex-row items-center justify-between">
         <TheLogo type="full" class="w-fit" width="150" height="48" />
-        <div class="flex flex-row items-center gap-4">
+        <div class="z-30 flex flex-row items-center gap-4">
           <Icon
             :name="$t('icons.whatsapp')"
             class="h-8 w-8 shrink-0 md:hidden" />
-          <Drawer>
-            <DrawerTrigger as-child>
-              <Button size="icon" variant="ghost">
+          <div>
+            <div>
+              <Button
+                size="icon"
+                variant="ghost"
+                @click="menuIsOpen = !menuIsOpen">
                 <Icon
-                  :name="$t('icons.menu')"
+                  :name="!menuIsOpen ? $t('icons.menu') : 'iconoir:xmark'"
                   class="h-8 w-8 shrink-0 md:hidden" />
               </Button>
-            </DrawerTrigger>
-            <DrawerContent>
-              <div class="mx-auto w-full max-w-sm">
-                <DrawerHeader>
-                  <DrawerTitle>
-                    <h3>
-                      {{ $t('pages.home.topBar.menu.title') }}
-                    </h3>
-                  </DrawerTitle>
-                  <DrawerDescription class="hidden">
-                    Selecione uma opção do menu
-                  </DrawerDescription>
-                </DrawerHeader>
-
-                <DrawerFooter>
-                  <DrawerClose as-child>
-                    <div class="">
-                      <ul class="flex w-full flex-col">
-                        <li v-for="item in Array(5).keys()" key="item">
-                          <NuxtLink
-                            :to="
-                              $t(`pages.home.topBar.menu.links.${item + 1}.url`)
-                            "
-                            :external="true">
-                            {{
-                              $t(
-                                `pages.home.topBar.menu.links.${item + 1}.label`,
-                              )
-                            }}
-                          </NuxtLink>
-                          <Separator class="my-4 w-full" />
-                        </li>
-                      </ul>
-                    </div>
-                  </DrawerClose>
-                </DrawerFooter>
-              </div>
-            </DrawerContent>
-          </Drawer>
+            </div>
+          </div>
 
           <div class="hidden flex-row gap-2 divide-x md:flex">
             <div
@@ -73,6 +39,37 @@
         </div>
       </div>
     </div>
+
+    <div
+      class="absolute inset-0 z-20 flex h-screen gap-5 bg-white/80 backdrop-blur-sm md:hidden"
+      v-if="menuIsOpen">
+      <div
+        class="mx-auto flex w-3/4 flex-col items-center justify-center text-primary">
+        <h3>
+          {{ $t('pages.home.topBar.menu.title') }}
+        </h3>
+
+        <div class="w-full">
+          <ul class="flex w-full flex-col text-center">
+            <li
+              v-for="item in Array(5).keys()"
+              key="item"
+              class="flex w-full flex-col">
+              <NuxtLink
+                class="w-full"
+                :to="$t(`pages.home.topBar.menu.links.${item + 1}.url`)"
+                :external="true"
+                @click="menuIsOpen = false">
+                {{ $t(`pages.home.topBar.menu.links.${item + 1}.label`) }}
+              </NuxtLink>
+              <Separator class="my-4 w-full" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </nav>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const menuIsOpen = ref(false);
+</script>
