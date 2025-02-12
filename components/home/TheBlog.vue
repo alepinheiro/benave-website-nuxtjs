290f957c13dc31bc68679e6876fe0e6cfa7252a9
Alessandro Pinheiro <alessandropsbra@gmail.com>
2025-02-12 19:45:44 -0300

    fix: update blog section

--- components/home/TheBlog.vue
+++ components/home/TheBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="blog" class="px-5 py-12">
-    <div class="mx-auto flex max-w-7xl flex-col gap-5 md:flex-row">
-      <div class="flex flex-col justify-around gap-2 md:items-start">
+    <div class="mx-auto flex max-w-7xl flex-col items-center gap-5">
+      <div class="flex flex-col justify-around gap-2 text-center">
         <div
           class="flex flex-row items-center justify-center gap-2 text-primary">
           <TheLogo type="icon" class="h-10 w-8" />
@@ -10,7 +10,7 @@
           </h2>
         </div>
 
-        <h3 class="pt-4 font-bold">
+        <h3 class="pt-4 text-xl font-bold">
           {{ $t('pages.home.blog.tagline') }}
         </h3>
 
@@ -18,20 +18,22 @@
           Esses temas destacam como a engenharia elétrica está conectada aos
           avanços tecnológicos e mudanças sociais.
         </p>
-
-        <Button asChild>
-          <NuxtLink to="/blog"> Acesse nosso Blog </NuxtLink>
-        </Button>
       </div>
+
       <template v-if="status === 'pending'">Carregando...</template>
       <div v-else-if="error">Erro: {{ error }}</div>
       <div
         v-else-if="posts"
-        class="flex max-w-xl flex-col gap-2 pt-4 md:max-w-3xl md:flex-row">
-        <BlogThePost :post="posts[0]" class="flex-1"> </BlogThePost>
-        <BlogThePost :post="posts[1]" class="flex-1"> </BlogThePost>
+        class="flex w-full flex-col gap-2 pt-4 md:flex-row">
+        <BlogThePost :post="posts[0]" class="flex-1" />
+        <BlogThePost :post="posts[1]" class="flex-1" />
+        <BlogThePost :post="posts[2]" class="flex-1" />
       </div>
       <div v-else>nenhum post</div>
+
+      <Button asChild class="w-fit">
+        <NuxtLink to="/blog"> Acesse nosso Blog </NuxtLink>
+      </Button>
     </div>
   </section>
 </template>
